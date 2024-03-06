0931cd49652db47fed08978e06869387b3fae51f
soundarya-ms3 <soundaryams03@gmail.com>
2024-03-06 21:54:23 +0530

    Input Shahe Effect

---
+++ INPUT_SHAKE_EFFECT/script.js
@@ -0,0 +1,14 @@
+function validateInput(){
+    let inputField =  document.getElementById("inputField");
+    let inputValue = inputField.value.trim();
+    let errorMessage = document.getElementById("errorMessage");
+    if(inputValue === ""){
+        inputField.classList.add("shake");
+        errorMessage.style.visibility ="visible";
+
+        setTimeout(function(){
+            inputField.classList.remove("shake");
+            errorMessage.style.visibility = "hidden";
+        },600)
+    }
+}
