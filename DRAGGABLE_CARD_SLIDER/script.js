7219aaa3b73e6343f3b70a55fb5b292e0ea93f8b
soundarya-ms3 <soundaryams03@gmail.com>
2024-03-16 19:48:19 +0530

    Draggable Card Slider

---
+++ DRAGGABLE_CARD_SLIDER/script.js
@@ -0,0 +1,25 @@
+const carousel  = document.querySelector(".carousel")
+
+let isDragging = false,startX,startScrollLeft;
+
+
+const dragStart =(e) =>{
+    isDragging = true;
+    carousel.classList.add("dragging");
+    startX = e.pageX;
+    startScrollLeft = carousel.scrollLeft;
+}
+
+const dragging = (e) =>{
+    if(!isDragging) return;
+    carousel.scrollLeft = e.pageX;
+}
+
+const dragStop = () =>{
+    isDragging = false;
+    carousel.classList.remove("dragging");
+}
+
+carousel.addEventListener("mousedown",dragStart);
+carousel.addEventListener("mousemove",dragging);
+document.addEventListener("mouseup", dragStop);
